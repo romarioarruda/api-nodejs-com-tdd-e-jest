a08bc5a55be04f50361290d37014ffb638056556
Romário Arruda <romarioarruda98@gmail.com>
2021-06-03 11:42:28 -0300

    add variaveis de ambiente

--- backend/infra/database.js
+++ backend/infra/database.js
@@ -1,11 +1,11 @@
 const pgp = require('pg-promise')()
 
 const db = pgp({
-    user: 'postgres',
-    password: '',
-    host: '127.0.0.1',
-    port: 5432,
-    database: 'blog'
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE || 'blog'
 })
 
 module.exports = db
